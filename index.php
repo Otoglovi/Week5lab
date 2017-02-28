16f43ecea90eed4b825b52f6665a818929712737
Otoglovi <darlington.2004@gmail.com>
2017-02-28 17:02:01 +0000

    week5lab

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
             $myname = "Frodo Baggins";
             $myage = 111;
 
-            echo "My name is" .$myname."and I am ".$myage;
+            echo "<br/> My name is " .$myname." and I am ".$myage;
             ?>
         </p>
     </body>
