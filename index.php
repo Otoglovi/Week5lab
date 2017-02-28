f7f38d48905ea18f0cf2e4016954f93e6cb298ac
Otoglovi <darlington.2004@gmail.com>
2017-02-28 18:50:57 +0000

    week5lab

--- index.php
+++ index.php
@@ -13,9 +13,13 @@
             $myname = "Frodo Baggins";
             $myage = 111;
 
-            echo "<br/><br/> My name is " .$myname." and I am ".$myage;
+            echo "<br/> My name is " .$myname." and I am ".$myage;
 
+            echo "I get printed";
+            // I don't! I'm a comment.
 
+            /* I don't get printed either
+            and neither do I*/
             ?>
         </p>
     </body>
