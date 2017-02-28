2137e7d70393d40d6b57716342878dfde8fa87d4
Otoglovi <darlington.2004@gmail.com>
2017-02-28 16:59:38 +0000

    week5lab

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
             echo "Hello World. This is my Time!!!";
             echo "<br/>Hello," . "" . "worldz" . "!<br/>";
             echo 5*7;
+
+            $myname = "Frodo Baggins";
+            $myage = 111;
+
+            echo "My name is" .$myname."and I am ".$myage;
             ?>
         </p>
     </body>
