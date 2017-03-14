c5115a17ee2430aa8b84f10dc79c5090474f01a6
Otoglovi <darlington.2004@gmail.com>
2017-03-14 15:03:09 +0000

    week5lab

--- index.php
+++ index.php
@@ -62,13 +62,13 @@
 
             switch ($wantedgood){
                 case 'specs':
-                    echo "You have to be 16 to buy mugs";
+                    echo "You have to be 16 to buy specs";
                     break;
                 case 'mugs':
-                    echo "You have to be 18 to buy mugs";
+                    echo "You have to be over 18 to buy mugs";
                     break;
                 case 'sausage rolls':
-                    echo "You have to be 18 to buy mugs";
+                    echo "You have to be over 21 to buy sausage rolls";
                     break;
                 default:
                     echo "Nothing available";
